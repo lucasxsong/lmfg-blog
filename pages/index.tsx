81226d6c4d64b3844f77734ab58b72eeb3b9f3a6
Lucas  Song <lucas7song@gmail.com>
2023-09-29 12:03:19 -0700

    restructure post

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>THE GARDEN BLOG</title>
         </Head>
         <Container>
           <Intro />
